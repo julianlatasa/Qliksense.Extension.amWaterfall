d55fcf560e0afa2079e8d79374556a6805ea19f6
Niels Lindberg <eighthourcream@gmail.com>
2016-06-27 11:45:02 +0200

    added zip to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,24 @@ module.exports = function(grunt) {
                 files: [{
                     expand: false,
                     cwd: '',
-                    src: ['amWaterfall/amWaterfall.js','amWaterfall/amWaterfall.qext', 'amWaterfall/properties.js', 'amWaterfall/wbFolder.wbl'],
+                    src: ['amWaterfall/amWaterfall.js', 'amWaterfall/amWaterfall.qext', 'amWaterfall/properties.js', 'amWaterfall/wbFolder.wbl'],
                     dest: 'C:/Users/Niels/Documents/Qlik/Sense/Extensions/'
-                },
-                {
+                }, {
                     expand: false,
                     cwd: '',
-                    src: ['amCombo/amCombo.js','amCombo/amCombo.qext', 'amCombo/properties.js', 'amCombo/wbFolder.wbl'],
+                    src: ['amCombo/amCombo.js', 'amCombo/amCombo.qext', 'amCombo/properties.js', 'amCombo/wbFolder.wbl'],
                     dest: 'C:/Users/Niels/Documents/Qlik/Sense/Extensions/'
-                }
-                ]
+                }]
             }
         },
+        zip: {
+            'amWaterfall.zip': ['amWaterfall/amWaterfall.js', 'amWaterfall/amWaterfall.qext', 'amWaterfall/properties.js', 'amWaterfall/wbFolder.wbl'],
+            'amCombo.zip': ['amCombo/amCombo.js', 'amCombo/amCombo.qext', 'amCombo/properties.js', 'amCombo/wbFolder.wbl']
+        }
     });
 
+
+    grunt.loadNpmTasks('grunt-zip');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['copy']);
+    grunt.registerTask('default', ['copy', 'zip']);
 };
